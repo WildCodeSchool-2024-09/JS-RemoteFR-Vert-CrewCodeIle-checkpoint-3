6a317d2226294762d58e0d0057ef7a9d31169638
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-01-13 10:39:17 +0100

    finish step2

--- server/src/router.ts
+++ server/src/router.ts
@@ -14,6 +14,8 @@ import gameActions from "./modules/game/gameActions";
 
 router.post("/api/games", gameActions.add);
 
+import tileActions from "./modules/tile/tileActions";
+router.get("/api/tiles", tileActions.browse);
 /* ************************************************************************* */
 
 export default router;
