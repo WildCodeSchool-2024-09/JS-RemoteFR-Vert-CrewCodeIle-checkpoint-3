72f24424d1b393f2d10542dba9155c920e72e35f
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-01-13 12:27:04 +0100

    feat: finished step 4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from "express";
 import tileRepository from "./tileRepository";
+import { ne } from "@faker-js/faker/.";
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
@@ -11,7 +12,22 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  type validationErrors = {
+    field: string;
+    message: string;
+  };
+  const errors: validationErrors[] = [];
+  const { coord_x, coord_y } = req.body;
+  const fullCoord = await tileRepository.readByCoordinates(coord_x, coord_y);
+  if (fullCoord.length === 0) {
+    res.sendStatus(422);
+  }
+
+  if (errors.length === 0) {
+    next();
+  } else {
+    res.status(422);
+  }
 };
 
 export default {
