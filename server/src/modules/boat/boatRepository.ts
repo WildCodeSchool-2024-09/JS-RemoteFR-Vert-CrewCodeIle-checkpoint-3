31fedea7510df51bfa0a4d44d645ccb08ec211b9
Baczkiewicz Damien <baczkiewicz.damien@hotmail.fr>
2025-01-13 12:19:47 +0100

    step5

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -9,18 +9,29 @@ type Boat = {
   coord_y: number;
 };
 
+type Tile = {
+  id: number;
+  coord_x: number;
+  coord_y: number;
+  type: string;
+  has_treasure: boolean;
+};
+
 class BoatRepository {
   async readAll(where = {}) {
     const [rows] = await databaseClient.query<Rows>(
-      "select * from boat order by coord_y, coord_x",
+      `SELECT boat.id, boat.name, boat.coord_x, boat.coord_y, tile.type, tile.has_treasure
+       FROM boat
+       JOIN tile ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y
+       ORDER BY boat.coord_y, boat.coord_x`,
     );
 
-    return rows as Boat[];
+    return rows as (Boat & Tile)[];
   }
 
   async update(boatToUpdate: Partial<Boat>) {
     const [result] = await databaseClient.query<Result>(
-      "update boat set coord_x = ?, coord_y = ? where id = ? ",
+      "UPDATE boat set coord_x = ?, coord_y = ? WHERE id = ? ",
       [boatToUpdate.coord_x, boatToUpdate.coord_y, boatToUpdate.id],
     );
     return result.affectedRows;
