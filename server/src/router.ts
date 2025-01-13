4f7a314339a52ec1c17a438a1e64f69858e54653
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-01-13 10:25:29 +0100

    feat: finished step 2

--- server/src/router.ts
+++ server/src/router.ts
@@ -5,6 +5,8 @@ const router = express.Router();
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
+import tileActions from "./modules/tile/tileActions";
+router.get("/api/tiles", tileActions.browse);
 
 import boatActions from "./modules/boat/boatActions";
 
